979a636c62a19ac6def5a1a388741460608ab150
= <elkhalfaouizakaria@gmail.com>
2025-07-06 21:29:47 +0100

    the header is completed!

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,88 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './assets/logo.png';
+import sun from './assets/sun.svg';
 import BurgerMenu from './components/BurgerMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-regular-svg-icons';
 
 const App = () => {
-//   const [Change, setChange] = useState(0);
-//   const [Rank, setRank] = useState(0);
-//   const [Langs, setLangs] = useState([
-//     { id: 1, lang: '1' },
-//     { id: 2, lang: '2' },
-//     { id: 3, lang: '3' },
-//   ]);
-  const [Menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        // Scrolling down
+        if (!menu) {
+          // if the Menu is open don't lift the header up
+          setShowHeader(false);
+        }
+      } else {
+        // Scrolling up
+        setShowHeader(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div>
-      <header className="stiky top-0 left-0 w-full bg-white">
-        <div className="container m-auto px-4 grid grid-cols-1 lg:grid-cols-3">
-          <div className="col-span-1 flex justify-between items-center bg-white absolute lg:relative w-full lg:w-fit lg:h-fit left-0 top-0 px-4 py-2 lg:px-0 z-50">
-            {/* <div className="flex items-center gap-x-2"> */}
-            <img src={logo} alt="Gehmy Marine" className="w-14" />
-            {/* <h1 className="text-2xl">GEHMYMARINE</h1> */}
-            {/* </div> */}
-            <BurgerMenu Menu={Menu} setMenu={setMenu} />
+      <header
+        className={`sticky top-0 left-0 h-14 transition-transform duration-300 z-50 bg-zinc-100 border-b-2 border-zinc-200 ${
+          showHeader ? 'translate-y-0' : '-translate-y-full'
+        }`}
+      >
+        <div className="md:container mx-auto px-4 py-2 grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 items-center">
+          <div className="col-span-1 flex justify-between items-center md:p-0 bg-zinc-100">
+            <img src={logo} alt="Gehmy Marine" />
+            <div className="flex items-center gap-4">
+              <button className="md:hidden py-1 px-2 cursor-pointer rounded-full border-2 border-sky-700 bg-sky-700 hover:bg-zinc-100 text-zinc-100 hover:text-sky-700">
+                <FontAwesomeIcon icon={faSun} />
+              </button>
+              <button className="md:hidden w-fit py-1 px-3 cursor-pointer rounded-full border-2 border-sky-700 bg-sky-700 hover:bg-zinc-100 text-zinc-100 hover:text-sky-700">
+                Contact
+              </button>
+              <BurgerMenu menu={menu} setMenu={setMenu} />
+            </div>
           </div>
           <nav
-            className={`col-span-2 flex flex-col lg:flex-row justify-between py-6 lg:py-0 bg-white border-b-2 lg:border-0 border-sky-700 w-full h-fit lg:h-full absolute lg:relative left-0 lg:top-0 z-40 duration-500 ${
-              Menu ? 'gap-y-72 top-12' : 'gap-y-0 -top-72'
+            className={`md:col-span-4 lg:col-span-2 flex justify-between items-center flex-col md:flex-row py-12 md:py-0 absolute left-0 w-full md:h-fit md:relative transition-transform duration-300 -z-10 md:z-0 bg-zinc-100 ${
+              menu
+                ? 'h-[calc(100dvh-48px)] top-14 md:top-0 md:translate-0 border-b-6 md:border-y-0  border-b-sky-700'
+                : '-translate-y-full md:translate-0'
             }`}
           >
-            <ul className="flex flex-col lg:flex-row items-center gap-4 text-center">
-              <li className="hover:text-sky-700">Home</li>
-              <li className="hover:text-sky-700">Services</li>
-              <li className="hover:text-sky-700">Projects</li>
-              <li className="hover:text-sky-700">Equipments</li>
+            <ul className="flex flex-col md:flex-row items-center gap-4">
+              <li className="cursor-pointer text-zinc-800 hover:text-sky-700">
+                Home
+              </li>
+              <li className="cursor-pointer text-zinc-800 hover:text-sky-700 ">
+                Services
+              </li>
+              <li className="cursor-pointer text-zinc-800 hover:text-sky-700 ">
+                Projects
+              </li>
+              <li className="cursor-pointer text-zinc-800 hover:text-sky-700 ">
+                Equipments
+              </li>
             </ul>
-            <div className="flex flex-col lg:flex-row items-center gap-4 text-center">
-              {/* <button
-                onClick={() => {
-                  setRank(Rank - 1);
-                  let one = Langs[0];
-                  setTimeout(() => {
-                    setRank(0);
-                    setLangs([Langs[1], Langs[2], one]);
-                  }, 300);
-                }}
-              >
-                left
+            <div className="flex flex-col md:flex-row items-center gap-4">
+              <div className="">Lang</div>
+              <button className="hidden md:flex py-2 px-2 cursor-pointer rounded-full border-2 border-sky-700 bg-sky-700 hover:bg-zinc-100 text-zinc-100 hover:text-sky-700">
+                <FontAwesomeIcon icon={faSun} />
               </button>
-              <button
-                onClick={() => {
-                  setRank(Rank + 1);
-                  setInterval(() => {
-                    if(Change < 64) {
-                        setChange(Change + 8)
-                        document.getElementById('langs').style.right = Change + 'px';
-                    }
-                  }, 50);
-                  let three = Langs[2];
-                  setTimeout(() => {
-                    setRank(0);
-                    setLangs([three, Langs[0], Langs[1]]);
-                  }, 300);
-                }}
-              >
-                right
-              </button>
-              <div className="bg-orange-500 relative w-16 h-8 overflow-hidden">
-                <ul
-                  id="langs"
-                  className={`absolute top-0 left-1/2 h-full -translate-x-1/2 w-[300%] flex bg-green-400 ${
-                    Rank > 0
-                      ? 'right-16'
-                      : Rank < 0
-                      ? '-left-16'
-                      : ''
-                  }`}
-                >
-                  {Langs.map((lang) => (
-                    <li key={lang.id} className="w-16 bg-red-300">
-                      {lang.lang}
-                    </li>
-                  ))}
-                </ul>
-              </div> */}
-              <button className="flex items-center gap-2 px-3 py-1 border-2 border-sky-700 rounded-full cursor-pointer bg-sky-700 hover:bg-white text-white hover:text-sky-700">
+              <button className="flex items-center justify-center gap-2 w-72 md:w-fit py-2 md:py-1 px-4 md:px-3 cursor-pointer rounded-full border-2 border-sky-700 bg-sky-700 hover:bg-zinc-100 text-zinc-100 hover:text-sky-700">
                 <FontAwesomeIcon icon={faPaperPlane} />
                 <span>Contact</span>
               </button>
@@ -92,29 +90,152 @@ const App = () => {
           </nav>
         </div>
       </header>
-      <div className="absolute">
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,22 +1,22 @@
-const BurgerMenu = ({ Menu, setMenu }) => {
+const BurgerMenu = ({ menu, setMenu }) => {
   return (
     <ul
-      className="w-8 h-6 relative flex lg:hidden justify-end cursor-pointer"
-      onClick={() => setMenu(!Menu)}
+      className="w-8 h-6 relative flex md:hidden justify-end cursor-pointer"
+      onClick={() => setMenu(!menu)}
     >
       <li
         className={`absolute w-full h-1 bg-sky-700 rounded-full duration-300 ${
-          Menu ? 'top-1/2 -translate-y-1/2 -rotate-45' : 'top-0'
+          menu ? 'top-1/2 -translate-y-1/2 -rotate-45' : 'top-0'
         }`}
       ></li>
       <li
         className={`absolute h-1 bg-sky-700 rounded-full top-1/2 -translate-y-1/2 duration-300 ${
-          Menu ? ' w-0 ' : 'w-full'
+          menu ? ' w-0 ' : 'w-full'
         }`}
       ></li>
       <li
         className={`absolute w-full h-1 bg-sky-700 rounded-full  duration-300 ${
-          Menu ? 'bottom-1/2 translate-y-1/2 rotate-45' : 'bottom-0'
+          menu ? 'bottom-1/2 translate-y-1/2 rotate-45' : 'bottom-0'
         }`}
       ></li>
     </ul>
